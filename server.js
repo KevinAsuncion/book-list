c9f71c018e78d2eb2d562e958e4b6059a19517b5
KevinAsuncion <kevinramosasuncion@gmail.com>
2018-11-07 22:50:13 -0800

    updated routes added postgres

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mustacheExpress = require('mustache-express');
+const { Client } = require('pg');
 require('dotenv').config();
 
 const app = express(); 
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended: false}));
 
 const mustache = mustacheExpress(); 
 mustache.cache = null; 
@@ -12,13 +14,33 @@ mustache.cache = null;
 app.engine('mustache', mustache);
 app.set('view engine', 'mustache')
 
-
 app.get('/list-page', (req,res) => {
-    res.render('list-page')
+    res.render('list-page');
 })
 
-app.get('/book-form', (req,res) => {
-    res.render('book-form')
+app.get('/book/add', (req,res) => {
+    res.render('book-form');
+})
+
+app.post('/book/add', (req,res) => {
+    const {title, authors} = req.body;
+    //Connect to the Postgress
+    const client = new Client();
+    client.connect()
+        .then(()=>{
+            console.log('connection complete')
+            const sql = 'INSERT into books (title, authors) VALUES($1, $2)'
+            const params =[title, authors]
+            return client.query(sql, params)
+        })
+        .then((result) => {
+            console.log(result)
+            res.redirect('/list-page');
+        })
+        .catch((err) => {
+            console.log('err', err)
+            res.redirect('/list')
+        })
 })
 
 app.listen(process.env.PORT, () => {
